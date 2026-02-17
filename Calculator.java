d4318d4e6a168baf5e5e0bdeb166e915f1831c29
RimaAhmed1 <rimaaltarshi@gmail.com>
2026-02-17 11:28:12 +0400

    deleting the codes for a reason

--- Calculator.java
+++ Calculator.java
@@ -2,19 +2,7 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[]args){
-  Scanner scanner=new Scanner(System.in);
 
-  System.out.println("Enter first number: ");
-  int number1=scanner.nextInt();
-
-  System.out.println("Enter first number: ");
-  int number2=scanner.nextInt();
-
-  int sum=number1 + number2;
-  int difference=number1 - number2;
-
-        System.out.println("Sum: "+sum);
-        System.out.println("Difference: "+difference);
 
     }
 }
