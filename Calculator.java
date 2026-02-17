013593142a8d7f848095323a0947c35cb3ef020c
RimaAhmed1 <rimaaltarshi@gmail.com>
2026-02-17 15:13:16 +0400

    Math pow function is adding

--- Calculator.java
+++ Calculator.java
@@ -1,33 +1,27 @@
 import java.util.Scanner;
 
 public class Calculator {
-    public static void main(String[]args){
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         //Multiplication and division
-        System.out.println("Enter first number: ");
-        int num1 = scanner.nextInt();
-        System.out.println("Enter second number: ");
-        int num2 = scanner.nextInt();
+        System.out.println("Enter First Number: ");
+        int number1 = scanner.nextInt();
+        System.out.println("Enter Second Number: ");
+        int number2 = scanner.nextInt();
 
-        int product = num1 * num2;
-        int quotient = num1 / num2;
+        int product = number1 * number2;
+        int quotient = number1 / number2;
+        int sum = number1 + number2;
+        int difference = number1 - number2;
 
         System.out.println("product :" + product);
         System.out.println("Quotient :" + quotient);
 
-
-        System.out.println("Enter first number: ");
-        int number1 = scanner.nextInt();
-
-        System.out.println("Enter Second number: ");
-        int number2 = scanner.nextInt();
-
-        int sum = number1 + number2;
-        int difference = number1 - number2;
-
         System.out.println("Sum: " + sum);
         System.out.println("Difference: " + difference);
+
+        System.out.println(number1 + "is base and " + number2 + "is exponent and the RESULT is " + Math.pow(number1, number2));
 
 
     }
