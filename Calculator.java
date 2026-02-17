7beb1e39cb97e76c8ce9a66ee59a300209572b71
RimaAhmed1 <rimaaltarshi@gmail.com>
2026-02-17 14:57:49 +0400

    reformat the code

--- Calculator.java
+++ Calculator.java
@@ -5,29 +5,29 @@ public class Calculator {
 
         Scanner scanner = new Scanner(System.in);
         //Multiplication and division
-         System.out.println("Enter first number: ");
-         int num1 = scanner.nextInt();
-          System.out.println("Enter second number: ");
-          int num2 = scanner.nextInt();
+        System.out.println("Enter first number: ");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter second number: ");
+        int num2 = scanner.nextInt();
 
-         int product = num1 * num2;
-         int quotient = num1 / num2;
+        int product = num1 * num2;
+        int quotient = num1 / num2;
 
-         System.out.println("product :" + product);
-         System.out.println("Quotient :" + quotient);
+        System.out.println("product :" + product);
+        System.out.println("Quotient :" + quotient);
 
 
         System.out.println("Enter first number: ");
-        int number1=scanner.nextInt();
+        int number1 = scanner.nextInt();
 
-        System.out.println("Enter first number: ");
-        int number2=scanner.nextInt();
+        System.out.println("Enter Second number: ");
+        int number2 = scanner.nextInt();
 
-        int sum=number1 + number2;
-        int difference=number1 - number2;
+        int sum = number1 + number2;
+        int difference = number1 - number2;
 
-        System.out.println("Sum: "+sum);
-        System.out.println("Difference: "+difference);
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
 
 
     }
