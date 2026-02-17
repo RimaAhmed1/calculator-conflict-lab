5846e0c4d8265af930711dd90f3d071cbd66fc33
Ahed Albarhi <iiihed7@gmail.com>
2026-02-17 11:40:47 +0400

    new Multiplication and division

--- Calculator.java
+++ Calculator.java
@@ -2,7 +2,18 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        //Multiplication and division
+         System.out.println("Enter first number: ");
+         int num1 = scanner.nextInt();
+          System.out.println("Enter second number: ");
+          int num2 = scanner.nextInt();
 
+         int product = num1 * num2;
+         int quotient = num1 / num2;
+
+         System.out.println("product :" + product);
+         System.out.println("Quotient :" + quotient);
 
     }
 }
